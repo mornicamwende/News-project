bd9a65b5cf8428aedd7771e3e09f6b61ce3b4db3
Mornica Mwende <mornicamwende@gmail.com>
2020-09-20 21:59:04 +0300

    add manager.command to run the unit test

--- manage.py
+++ manage.py
@@ -10,7 +10,7 @@ manager = Manager(app)
 manager.add_command('server',Server)
 @manager.command
 def test():
-    """Run the unit tests."""
+    """Run the unit tests"""
     import unittest
     tests = unittest.TestLoader().discover('tests')
     unittest.TextTestRunner(verbosity=2).run(tests)
