62e3ceb718ec306ce018f1bc2df7cda8d13d153b
Mornica Mwende <mornicamwende@gmail.com>
2020-09-20 21:57:46 +0300

    add test_source file

--- tests/test_source.py
+++ tests/test_source.py
@@ -8,7 +8,7 @@ class SourcesTest(unittest.TestCase):
 
     def setUp(self):
         '''
-        Set up method that will run before every Test
+        Set up method that will run before every Test.
         '''
         self.new_source = Sources("abc-news","ABC News","Your trusted source for breaking news, analysis, exclusive interviews, headlines, and videos at ABCNews.com.","https://abcnews.go.com","general","en","us")
 
