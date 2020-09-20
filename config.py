65c6b145dd45dc8520c43b18ca4e5b354facef95
Mornica Mwende <mornicamwende@gmail.com>
2020-09-20 11:23:54 +0300

    deployment to heroku

--- config.py
+++ config.py
@@ -5,7 +5,6 @@ class Config:
     '''
     General configuration parent class
     '''
-    # MOVIE_API_BASE_URL ='https://api.themoviedb.org/3/movie/{}?api_key={}'
     NEWS_API_BASE_URL = 'https://newsapi.org/v2/sources?language=en&apiKey={}'
 
     ARTICLES_API_BASE_URL = 'https://newsapi.org/v2/everything?sources={}&apiKey={}'
